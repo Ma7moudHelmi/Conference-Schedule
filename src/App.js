fdcd823571e29c868121143ec0de47a9dacfefb0
IbrahimTagwifa <tagwifacode@gmail.com>
2022-04-02 15:16:13 +0200

    nav

--- src/App.js
+++ src/App.js
@@ -1,30 +1,8 @@
+import Nav  from "./nav";
 function App() {
   return (
     <>
-     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Conference Schedule</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarText">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Location Info</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Food Info</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Schedule</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+     <Nav />
     </>
   );
 }
