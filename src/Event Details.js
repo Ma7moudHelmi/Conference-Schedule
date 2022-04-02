ecd27367c1d5c1354a867ef8d30810f38b26efb3
Ma7moudHelmi <mahmoudhelmy31@gmail.com>
2022-04-02 15:05:12 +0200

    edit

---
+++ src/Event Details.js
@@ -0,0 +1,15 @@
+import "./App.css"
+export function Details(title,Description,place,speaker) {
+  return (
+    // <div className="Event ">
+    //   <h1>Apple Events</h1>
+    //   <p>
+    //     An Apple event is a high-level event that conforms to the Apple Event
+    //     Interprocess Messaging Protocol. The Apple Event Manager uses the
+    //     services of the Event Manager to send Apple events between applications
+    //     on the same computer
+    //   </p>
+    //   <p>the event will be in April,21 2022 At NewYork City</p>
+    // </div>
+  );
+}
